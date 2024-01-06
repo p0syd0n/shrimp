77370bb330d0312e564c31665d9be84e6b14c2d9
BuildTools <unconfigured@null.spigotmc.org>
2024-01-06 23:52:11 +0000

    IT WORKS

---
+++ modules/default.py
@@ -0,0 +1,5 @@
+import requests
+
+def main():
+    print("Default module: importing of requests and printing/returning of this message")
+    return "Default module: importing of requests and printing/returning of this message" 
